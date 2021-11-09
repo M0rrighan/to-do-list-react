9faf60c6e699c146a38f1aa880de1025161c4945
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-11-09 23:34:41 +0100

    Add useState to hold inputField state and function onChange, add handleSubmit function

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function InputTodo() {
+function InputTodo(props) {
+  const [inputField, setInputField] = useState({
+    title: "",
+  });
+
+  const onChange = e => {
+    setInputField({
+      // ...inputField,
+      title: e.target.value,
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(inputField.title.trim()) {
+      props.addTodoItem(inputField.title);
+      setInputField({
+        title: "",
+      })
+    } else {
+      alert("ToDo Title cannot be empty");
+    } 
+  };
   
   return (
-    <div>
+      <form onSubmit={handleSubmit}>
       <input 
-      type="text" 
-      placeholder="Add to do...."
+        type="text" 
+        placeholder="Add to do...."
+        value={inputField.title}
+        onChange={onChange}
       />
       <button>+</button>
-    </div>
-  )
+      </form>
+  );
 }
 
-export default InputTodo
+export default InputTodo;
