680cdb4096450195616b6e5b5d15144ffb3534ad
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-11-11 03:22:08 +0100

    Edit Navbar: use State and add function to toggle navbar open/close

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from "react";
 import { NavLink  } from "react-router-dom";
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
 
 const Navbar = () => {
 const links = [
@@ -15,12 +17,28 @@ const links = [
   },
 ];
 
+const [navbarOpen, setNavbarOpen] = useState(false);
+
+const handleToggle = () => {
+  setNavbarOpen(prev => !prev)
+};
+
+
   return (
     <nav className="navBar">
-      <ul>
+      <button onClick={handleToggle}>
+        { navbarOpen ? ( 
+            <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} /> 
+          ) : (
+            <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+          ) }
+      </button>
+      <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
         {links.map(link => {
           return <li key={link.id}>
-                  <NavLink to={link.path}>{link.text}</NavLink >
+                  <NavLink to={link.path} onClick={handleToggle}>
+                    {link.text}
+                  </NavLink >
                  </li>
         })}
       </ul>
